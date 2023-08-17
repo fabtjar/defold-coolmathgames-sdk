3a9105508eb30771a4de1bd93d4c2ae3927e2598
Fab <fabianjarrett@gmail.com>
2023-08-17 08:29:14 +0100

    Add logging for game event

--- coolmathgames/lib/web/lib_coolmathgames_sdk.js
+++ coolmathgames/lib/web/lib_coolmathgames_sdk.js
@@ -3,8 +3,15 @@
 var LibCoolmathGamesSdk = {
 
     CoolmathGamesSdkJs_cmgGameEvent: function(cm_game_evt, cm_game_lvl) {
+        console.log("*** CoolmathGamesSdkJs_cmgGameEvent ***");
+        console.log("before:");
+        console.log("cm_game_evt:", cm_game_evt);
+        console.log("cm_game_lvl:", cm_game_lvl);
         cm_game_evt = UTF8ToString(cm_game_evt);
         cm_game_lvl = UTF8ToString(cm_game_lvl);
+        console.log("after:");
+        console.log("cm_game_evt:", cm_game_evt);
+        console.log("typeof cm_game_lvl:", typeof cm_game_lvl);
         if (parent.cmgGameEvent) {
             if (cm_game_lvl) {
                 parent.cmgGameEvent(cm_game_evt, cm_game_lvl);
@@ -12,6 +19,7 @@ var LibCoolmathGamesSdk = {
                 parent.cmgGameEvent(cm_game_evt);
             }
         }
+        console.log("*** * ***");
     },
 
     CoolmathGamesSdkJs_cmgAdBreak: function() {

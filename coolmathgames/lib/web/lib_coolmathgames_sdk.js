ea80a3ee10a6c46284078e03b51db39a8d384353
Fab <fabianjarrett@gmail.com>
2023-08-17 08:49:39 +0100

    Start game event fix (#1)
    * Set game level to undefined if null pointer

--- coolmathgames/lib/web/lib_coolmathgames_sdk.js
+++ coolmathgames/lib/web/lib_coolmathgames_sdk.js
@@ -4,13 +4,12 @@ var LibCoolmathGamesSdk = {
 
     CoolmathGamesSdkJs_cmgGameEvent: function(cm_game_evt, cm_game_lvl) {
         cm_game_evt = UTF8ToString(cm_game_evt);
-        cm_game_lvl = UTF8ToString(cm_game_lvl);
+
+        // Only convert to string if it's not a null pointer.
+        cm_game_lvl = cm_game_lvl ? UTF8ToString(cm_game_lvl) : undefined;
+
         if (parent.cmgGameEvent) {
-            if (cm_game_lvl) {
-                parent.cmgGameEvent(cm_game_evt, cm_game_lvl);
-            } else {
-                parent.cmgGameEvent(cm_game_evt);
-            }
+            parent.cmgGameEvent(cm_game_evt, cm_game_lvl);
         }
     },
 

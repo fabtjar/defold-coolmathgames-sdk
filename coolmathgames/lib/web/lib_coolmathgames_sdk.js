17983e1f7d26832500f94023559a9eb6bef6eba2
Fab <fabianjarrett@gmail.com>
2023-08-17 08:46:58 +0100

    Clean up

--- coolmathgames/lib/web/lib_coolmathgames_sdk.js
+++ coolmathgames/lib/web/lib_coolmathgames_sdk.js
@@ -3,27 +3,14 @@
 var LibCoolmathGamesSdk = {
 
     CoolmathGamesSdkJs_cmgGameEvent: function(cm_game_evt, cm_game_lvl) {
-        console.log("*** CoolmathGamesSdkJs_cmgGameEvent ***");
-
         cm_game_evt = UTF8ToString(cm_game_evt);
 
         // Only convert to string if it's not a null pointer.
         cm_game_lvl = cm_game_lvl ? UTF8ToString(cm_game_lvl) : undefined;
 
-        if (cm_game_lvl) {
-            console.log("cm_game_lvl:", cm_game_lvl, "valid");
-        } else {
-            console.log("cm_game_lvl:", cm_game_lvl, "undefined");
-        }
-
         if (parent.cmgGameEvent) {
-            if (cm_game_lvl) {
-                parent.cmgGameEvent(cm_game_evt, cm_game_lvl);
-            } else {
-                parent.cmgGameEvent(cm_game_evt);
-            }
+            parent.cmgGameEvent(cm_game_evt, cm_game_lvl);
         }
-        console.log("*** * ***");
     },
 
     CoolmathGamesSdkJs_cmgAdBreak: function() {
